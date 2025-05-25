51345dc97291c356b3bb93ca6866109123709ac9
DISSANAYAKE D M D C <161795577+it23782518@users.noreply.github.com>
2025-05-26 03:06:08 +0530

    Update WebConfig.java

--- src/main/java/com/example/Backend/config/WebConfig.java
+++ src/main/java/com/example/Backend/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://gms-f-8ppq.vercel.app", "https://gms-f1-ooad.vercel.app")
+                .allowedOriginPatterns("https://gms-f-8ppq.vercel.app", "https://gms-f1-ooad.vercel.app" , "https://mansa-brown.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
